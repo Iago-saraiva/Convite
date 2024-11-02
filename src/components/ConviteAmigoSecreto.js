9ffa0f60d6cfecb9575aea059f9ec7c63dbf7987
Iago Carvalho <iagosaraiva396@gmail.com>
2024-11-02 12:51:25 -0300

    primeiiro

---
+++ src/components/ConviteAmigoSecreto.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+
+const ConviteAmigoSecreto = () => {
+    const [destinatario, setDestinatario] = useState('');
+    const [nomeParticipante, setNomeParticipante] = useState('');
+
+    const enviarConvite = (e) => {
+        e.preventDefault();
+
+        const assunto = encodeURIComponent('Convite para o Amigo Secreto');
+        const corpo = encodeURIComponent(`Olá!\n\nVocê foi convidado para participar do amigo secreto.\nO participante que te tirou foi ${nomeParticipante}.\n\nParticipe e divirta-se!`);
+
+        const mailtoLink = `mailto:${destinatario}?subject=${assunto}&body=${corpo}`;
+
+        window.location.href = mailtoLink;
+
+        setDestinatario('');
+        setNomeParticipante('');
+    };
+
+    return (
+        <div>
+            <form onSubmit={enviarConvite}>
+                <h1>Convite para Amigo Secreto</h1>
+                <label htmlFor="destinatario">E-mail do Destinatário:</label>
+                <input
+                    type="email"
+                    id="destinatario"
+                    value={destinatario}
+                    onChange={(e) => setDestinatario(e.target.value)}
+                    required
+                />
+                <br /><br />
+
+                <label htmlFor="nomeParticipante">Seu Nome:</label>
+                <input
+                    type="text"
+                    id="nomeParticipante"
+                    value={nomeParticipante}
+                    onChange={(e) => setNomeParticipante(e.target.value)}
+                    required
+                />
+                <br /><br />
+
+                <button type="submit">Enviar Convite</button>
+            </form>
+            <a className='desejos' href="https://iago-saraiva.github.io/lista-desejos/" target="_blank" rel="noopener noreferrer">
+        Lista de desejos
+        </a>
+        </div>
+    );
+};
+
+export default ConviteAmigoSecreto;
